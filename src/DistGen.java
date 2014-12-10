503ee8df4ac8f3ab6edf316bfbe7f97c77998c8c
Mark Shanoudy <mathis402@gmail.com>
2014-12-10 04:38:05 -0500

    Cleaned up DistGen and removed DistGenDriver
    - Removed unused methods

--- src/DistGen.java
+++ src/DistGen.java
@@ -1,197 +1,45 @@
 import java.util.Random;
 import java.util.*;
 
-public class DistGen
+public class DistGen extends Random
 {
-	private Random random;
-	
-	/**
-     * Default Constructor
-     */
-	public DistGen()
+	public int nextPoisson(double lambda)
 	{
-		random = new Random();
-	}
+  		double elambda = Math.exp(-1 * lambda);
+		double product = 1;
+		int count  = 0;
+		int result = 0;
 
-	/**
-	 * Constructor which accepts an RNG seed
-	 *
-	 * @param seed The initial seed
-	 */
-	public DistGen(long seed)
-	{
-		random = new Random(seed);
-	}
-	
-	/**
-     * Generates random sorted arrival times from the possion process dist using exponential interarrival times in seconds
-     * The time is in seconds
-     *
-     * @param arrivalRate the arrival used to gen the random numbers
-     * @param T the total time to gen times for(use seconds 3600 for hour)
-	 *
-     * @return a sorted int[]
-     */
-	public int[] genPoissonProc( int arrivalRate, int T)
-	{
-		int t = 0;
-		int x = 0;
-		
-		int[] times = new int[T];
-		
-		while ( t < T)
+		while (product >= elambda)
 		{
-			t += (int)genExp(arrivalRate,false,0.0);		
-			
-			if(t <= T )
-			{
-				times[x] = t;
-				x++;
-			}
-		
+			product *= nextDouble();
+			result = count;
+			count++;
 		}
-		
-		times = Arrays.copyOf(times,x);
-		
-		return times;
-	}
-	
-	/**
-     * Generates random numbers from the exponential dist
-     *
-     * @param lambda the lamba/arrival rate to use in the inverse transform method
-     * @param unitFlag true = R with decimals, false = R scaled to seconds from uniform
-	 * @param limiter for use when needed unit is seconds, will cause output of a second time value from [0,limiter)--use 0.0 for no range
-	 *
-     * @return a single double from the exponential dist(if secs needed cast int)
-     */
-	public double genExp( double lambda, boolean unitFlag, double limiter)
-	{
-		boolean flag = false;				//Flag for generating a ran number in a range
-		double R;
-		double RSec;
-				
-		//Generate a ran num useing inverse transform
-		//If a random number in unit of seconds needs to be generated in a range
-		//it will continue generating a random time unit untill it is in the range
-		while(true)
-		{
-			R = random.nextDouble();
-			
-			R = 1.0 - R;
-			
-			R = Math.log(R) *-1.0;
-			R = R/lambda;
-			
-			if(unitFlag == true)
-				break;
-			
-			R = R*60.0* 60.00;
-			
-			//Trim needless decimals
-			R = (int)R;
-			
-			if(limiter == 0.0)
-				break;
-			
-			if(R <= limiter)
-				break;
-		}
-		
-		return R;
+
+		return result;
 	}
 
-	/**
-     * Generates random numbers from the normal dist wil the specified mean and std
-     * @param mean values will cluster around this
-     * @param stD the standard deviation
-     * @return an int from the normal
-     */
-	public int genNormal( double mean, double stD)
+	public int[] poissonProcess(int T, int n)
 	{
+		int array[] = new int[n];
 
-		
-		int value = (int)((random.nextGaussian()*stD) + mean);
-		
-		//Assumption: no 0 or negative items or time
-		while (value <= 0)
-		{
-			value = (int)((random.nextGaussian()*stD) + mean);
-		}
-		
-		return value;
-	}
-	
-	
-	/**
-     * Generates random numbers from the uniform dist to check against a p value
-     * @param p the p value to check against
-     * 
-     * @return if(R <= p) true(qz), if R>p return false(mtg)
-     */
-	public boolean genBern(double p)
-	{
+		for (int x = 0; x < array.length; x++)
+			array[x] = (int)nextDouble() * T;
+		Arrays.sort(array);
 
-		
-		return random.nextDouble() <= p;
-	
+		return array;
 	}
-	
-	/**
-     * Generates arrival times following a poisson process using uniform interarrival times(USE THIS ONE!) in seconds
-     * @param arrivalRate the lambda
-	 * @param T The time period
-     * 
-     * @return arrival times in the specified time period
-     */
-	public int[] genPoissonProc2(double arrivalRate, double T)
+
+	public int nextGaussian(double mean, double standardDeviation)
 	{
-		//ArrayList<Integer> arrivalTimes =  new ArrayList<Integer>();
-		
-		double N = genPoissonDist(arrivalRate,T);
-		
-		int[] arrivalTimes = new int[(int)N];
-		
-		for(int i=0; i < N; i++)
-		{		
-			arrivalTimes[i] = (int)(random.nextDouble()*T);
-		}
-	
-		Arrays.sort(arrivalTimes);
-		
-		return arrivalTimes;
-	}
-	
-	/**
-     * Generates random number of arrivals from the poisson dist
-     * @param arrivalRate the lambda
-	 * @param T The time period
-     * 
-     * @return the number of arrivals in that time period
-     */
-	public double genPoissonDist(double arrivalRate, double T)
-	{
-		
-		double L = Math.exp(-1.0*arrivalRate);
-		double K = 0.0;
-		
-		double p = 1.0;
-		
-		//System.out.println(L);
-		
+		int value;
+
 		do
 		{
-			K = K +1.0;
+			value = (int) ((nextGaussian() * standardDeviation) + mean);
+		} while(value <= 0);
 
-			p = p* random.nextDouble();
-			
-			//System.out.println(p);
-		
-		}while( p > L);
-	
-		//System.out.println( K-1.0);
-	
-		return K-1.0;
-		
+		return value;
 	}
 }

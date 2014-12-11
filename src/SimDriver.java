30a60825c7001445bc5f9fad385c9b707874711a
Steve Ross <smr0327@gmail.com>
2014-12-10 20:25:27 -0500

    Updated driver with command line args and error checking

--- src/SimDriver.java
+++ src/SimDriver.java
@@ -1,12 +1,33 @@
+import java.io.*;
 
 public class SimDriver
 {
-	private static final int QZ_OPERATING_HOURS = 13;
-
-	public static void main(String args[])
+	public static void main(String args[]) throws IOException
 	{
+		if((args.length < 3))
+		{
+			throw new IOException("User input error. Please use Proper command Line Arguments(See readme file)");
+		}
+		
+		int operatingHours = Integer.parseInt(args[0]);
+		
+		if( operatingHours < 1)
+		{
+			throw new IOException("User input error. Please use time greater than or equal to one hour(See readme file)");
+		}
+		
+		//num of checkout cashiers(servers)
+		int numOfCS = Integer.parseInt(args[1]);
+		
+		//Num of mtg servers
+		int numOfMTGServer = Integer.parseInt(args[2]);
+		
+		if(numOfCS < 1 | numOfMTGServer < 1)
+		{
+			throw new IOException("User input error. One of the server specs is invalid(See readme file)");
+		}
+
 		SimulationEngine simulationEngine = new SimulationEngine();
-		//simulationEngine.runSimulation(1, 1, 2);
-		simulationEngine.runSimulation(QZ_OPERATING_HOURS, 1, 4);
+		simulationEngine.runSimulation(operatingHours, numOfMTGServer, numOfCS);
 	}
 }
